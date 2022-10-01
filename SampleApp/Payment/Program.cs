4fa7661a1224b552101ca0b4b16fdb2b58888b09
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2022-10-01 23:53:57 +0530

    Adding sample .NET application

---
+++ SampleApp/Payment/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api", async () =>
+ {
+     using var scope = app.Services.CreateScope();
+     var httpClient = (scope.ServiceProvider.GetRequiredService<IHttpClientFactory>()).CreateClient();
+     var response = await httpClient.GetAsync("https://dummyjson.com/todos");
+     var result = await response.Content.ReadAsStringAsync();
+     return result;
+ });
+
+app.Run();
